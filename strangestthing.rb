26309f3ef40e203802210c4eeadc145ef0e65758
JackOfHearts318 <45011748+JackOfHearts318@users.noreply.github.com>
2020-01-17 13:44:51 -0800

    Add files via upload

---
+++ strangestthing.rb
@@ -0,0 +1,20 @@
+# Welcome to Sonic Pi v3.1
+live_loop :piaNo do
+  use_synth :piano
+  play :C
+  sleep 0.20
+  play :E
+  sleep 0.20
+  play :G
+  sleep 0.20
+  play :B
+  sleep 0.20
+  play :C5
+  sleep 0.20
+  play :B
+  sleep 0.20
+  play :G
+  sleep 0.20
+  play :E
+  sleep 0.20
+end
