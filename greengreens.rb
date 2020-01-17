9efe60ba7111b85283900bf59e724cf559cdcaa9
JackOfHearts318 <45011748+JackOfHearts318@users.noreply.github.com>
2020-01-17 14:04:20 -0800

    Add files via upload

---
+++ greengreens.rb
@@ -0,0 +1,72 @@
+# Green Greens from Kirby's Dream Land: Composed by Jun Ishikawa
+
+#Intro
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+sleep 0.08
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+sleep 0.08
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", amp: 2
+sleep 0.10
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", amp: 2
+sleep 0.10
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", amp: 2
+sleep 0.40
+
+2.times do
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass.wav", rate: 0.9, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.18
+  sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.1, amp: 2
+  sleep 0.20
+end
+
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+sleep 0.35
+
+use_synth :chipbass
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+play :F, sustain: 0.1, amp: 0.5
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.2, amp: 2
+play :F, sustain: 0.1, amp: 0.5
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+play :G, sustain: 0.1, amp: 0.5
+sleep 0.18
+sample "C:/Users/jackson_atlas/Desktop/seinfeldbass3.wav", rate: 1.33, amp: 2
+play :G, sustain: 0.1, amp: 0.5
+sleep 0.18
+
