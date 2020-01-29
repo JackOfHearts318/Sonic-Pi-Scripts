b761a356916d655d2d17807547689d43b2f4c66d
JackOfHearts318 <45011748+JackOfHearts318@users.noreply.github.com>
2020-01-29 08:58:18 -0800

    Add files via upload

---
+++ evenstrangerthings.rb
@@ -0,0 +1,21 @@
+use_bpm 160
+
+live_loop :piaNo do
+  use_synth :chipbass
+  play :C
+  sleep 0.5
+  play :E
+  sleep 0.5
+  play :G
+  sleep 0.5
+  play :B
+  sleep 0.5
+  play :C5
+  sleep 0.5
+  play :B
+  sleep 0.5
+  play :G
+  sleep 0.5
+  play :E
+  sleep 0.5
+end
